8944f092f84700f910734fff4743c16720d55c7d
Unknown <AThompson027@west-mec.org>
2018-08-16 15:25:17 -0700

    Started
    Just started the project.

---
+++ script.js
@@ -0,0 +1,19 @@
+ /*
+      Project 01_06_01
+
+      Author: Annie Thompson
+      Date: 8-16-18
+
+      Filename: script.js
+   */
+
+"use strict";
+
+var formValidity = true;
+
+if (window.addEventListener) {
+    window.addEventListener("load", createElementListeners, false);
+}
+
+
+   
