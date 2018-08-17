5734eb7bd334a176831a51feed0f02454773ea01
Unknown <AThompson027@west-mec.org>
2018-08-16 23:06:28 -0700

    Submit
    Made the HTML shut off the form's validation and made functions and event handlers to stop the default submission of the form.

--- script.js
+++ script.js
@@ -8,12 +8,34 @@
    */
 
 "use strict";
-
+// global variable
 var formValidity = true;
 
-if (window.addEventListener) {
-    window.addEventListener("load", createElementListeners, false);
+// function to prevent the form from submitted on default
+function validateForm(submit) {
+  if (submit.preventDefault) {
+        submit.preventDefault();
+    } else {
+        submit.returnValue = false;
+    }
+    formValidity = true;
+
+    createEventListeners();
 }
 
+// function to create event listeners for the form
+function createEventListeners() {
+  var form = document.getElementsByTagName("form")[0];
+  if (form.addEventListener) {
+      form.addEventListener("submit", validateForm, false);
+  } else if (form.attachEvent) {
+      form.attachEvent("onsubmit", validateForm);
+  }
+}
 
-   
+// event listeners for window when it loads
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
